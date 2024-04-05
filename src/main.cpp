3bae63e567cf0778a85264531d14c34f7f057d94
0mgRod <rodrigofonseca20@outlook.com>
2024-04-05 14:51:52 +0100

    v1.3.0-beta.3

--- src/main.cpp
+++ src/main.cpp
@@ -247,13 +247,13 @@ class $modify(GJGarageLayer) {
 			auto demonIcon = CCSprite::createWithSpriteFrameName("GJ_demonIcon_001.png");
 			demonIcon->setPosition({winSize.width - 18, winSize.height - 117});
 			demonIcon->setAnchorPoint({0.5, 0.5});
-			demonIcon->setScale({0.5});
+			demonIcon->setScale(0.5);
 			demonIcon->setID("demons-icon");
 
 			CCDictionary* stats = GameStatsManager::sharedState()->m_playerStats;
 			CCLabelBMFont* demonText = CCLabelBMFont::create(std::to_string(stats->valueForKey("5")->intValue()).c_str(), "bigFont.fnt");
 			demonText->setPosition(CCPoint(demonIcon->getPositionX() - 12, this->getChildByID("diamond-shards-label")->getPositionY() - 15));
-			demonText->setScale({0.34});
+			demonText->setScale(0.34);
 			demonText->setAnchorPoint({1, 0.5});
 			demonText->setID("demons-label");
 
@@ -270,7 +270,7 @@ class $modify(GJGarageLayer) {
                 demonIcon->setPosition({winSize.width - 18, winSize.height - 117});
             }
             demonIcon->setAnchorPoint({0.5, 0.5});
-            demonIcon->setScale({0.5});
+            demonIcon->setScale(0.5);
             demonIcon->setID("cp-icon");
             this->addChild(demonIcon);
 
