e590e73aa9306cae6908bd9f1e6ad4a34bb2003c
0mgRod <rodrigofonseca20@outlook.com>
2024-06-08 21:33:25 +0100

    test 1 for ` v3.0.0-alpha.1 `

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,29 @@
+#include <chrono>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GJGarageLayer.hpp>
+#include <Geode/binding/GJGarageLayer.hpp>
 #include <Geode/utils/web.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
-class GPFeedbackLayer : public CCLayer
-{
+class GPFeedbackLayer : public CCLayer {
 public:
-
-    static CCScene* scene()
-    {
+    static CCScene* scene() {
         auto scene = CCScene::create();
         scene->addChild(GPFeedbackLayer::create());
         return scene;
     }
 
-    static CCScene* garageScene()
-    {
+    static CCScene* garageScene() {
         auto scene = CCScene::create();
-        scene->addChild(GJGarageLayer::node());
+        scene->addChild(GJGarageLayer::scene());
         return scene;
     }
-    
-    static GPFeedbackLayer* create()
-    {
+
+    static GPFeedbackLayer* create() {
         GPFeedbackLayer* ret = new GPFeedbackLayer();
-        if(ret && ret->init())
-        {
+        if (ret && ret->init()) {
             ret->autorelease();
             return ret;
         }
@@ -35,105 +31,88 @@ public:
         return nullptr;
     }
 
-    virtual void keyBackClicked()
-    {
+    virtual void keyBackClicked() {
         onClick(nullptr);
     }
 
-    void onFeedbackClick(CCObject* sender)
-    {
-        // Get descInput node by ID
-        auto descInput = dynamic_cast<CCTextInputNode*>(this->getChildByID("descInput")); // Assuming descInput tag is 2
+    void onFeedbackClick(CCObject* sender) {
+        auto descInput = dynamic_cast<CCTextInputNode*>(this->getChildByID("descInput"));
         if (!descInput)
             return;
 
-        std::string feedback = descInput->getString(); // Get the text from descInput
+        std::string feedback = descInput->getString();
+        std::string username = "Anonymous";
 
-        gd::string gdUsername; // Declare outside the if-else blocks
-
-        // Assuming gd::string is returned by GJAccountManager::get()->m_username
         if (GJAccountManager::get()->m_accountID != 0) {
-            gdUsername = GJAccountManager::get()->m_username;
-        } else {
-            gdUsername = "Anonymous";
+            username = std::string(GJAccountManager::get()->m_username.c_str());
         }
 
-        // Convert gd::string to std::string
-        std::string username = std::string(gdUsername.c_str());
-
-        // Now use the username in constructing the payload
         std::string payload = "{\"content\": \"" + username + " said:\\n\\n" + feedback + "\",\"embeds\": null,\"attachments\": []}";
 
-        // Send the JSON payload to the Discord webhook URL
-        web::AsyncWebRequest()
-            .method("POST")
-            .contentType("application/json")
-            .bodyRaw(payload)
-            .fetch("https://canary.discord.com/api/webhooks/1225440650988884029/NPsyWQuLi6x3GB-DymtlIvVIlzC0Gm0qocSxbq9wILUhnjZsGR5Rc6YEY2mEtdpvQ0x_")
-            .text()
-            .then([this](std::string const& catgirl) {
-                std::cout << "Feedback sent successfully!" << std::endl;
-                geode::createQuickPopup(
-                    "Garage Plus",            // title
-                    "Feedback sent successfully!",   // content
-                    "Exit", nullptr,      // buttons
-                    [this](auto, bool btn1) {
-                        this->onClick(nullptr);
-                    }
-                );
+        web::WebRequest request;
+        // request.method("POST");
+        request.header("Content-Type", "application/json");
+        request.bodyString(payload);
+        request.timeout(std::chrono::seconds(30));
 
+        std::string url = "https://canary.discord.com/api/webhooks/1225440650988884029/NPsyWQuLi6x3GB-DymtlIvVIlzC0Gm0qocSxbq9wILUhnjZsGR5Rc6YEY2mEtdpvQ0x_";
 
-            })
-            .expect([this](std::string const& error) {
-                std::cerr << "Failed to fetch anything: " << error << std::endl;
-                geode::createQuickPopup(
-                    "Garage Plus",            // title
-                    "Failed to fetch anything: " + error,   // content
-                    "Retry", "Exit",      // buttons
-                    [this](auto, bool btn1) {
-                        if (btn1) {
-                            this->onFeedbackClick(nullptr);
-                        } else {
+        m_listener.bind([this](web::WebTask::Event* e) {
+            if (web::WebResponse* res = e->getValue()) {
+                if (res->ok()) {
+                    std::cout << "Feedback sent successfully!" << std::endl;
+                    geode::createQuickPopup(
+                        "Garage Plus",
+                        "Feedback sent successfully!",
+                        "Exit", nullptr,
+                        [this](auto, bool btn1) {
                             this->onClick(nullptr);
                         }
-                    }
-                );
-            });
+                    );
+                } else {
+                    std::cerr << "Request failed with status code: " << res->code() << std::endl;
+                }
+            } else if (web::WebProgress* progress = e->getProgress()) {
+                // std::cout << "Progress: " << progress->downloadProgress() << std::endl;
+            } else if (e->isCancelled()) {
+                std::cerr << "The request was cancelled... So sad :(" << std::endl;
+            }
+        });
+
+        m_listener.setFilter(request.post(url));
     }
 
-    bool init()
-    {
+    bool init() {
         if (!CCLayer::init())
             return false;
 
         auto winSize = CCDirector::sharedDirector()->getWinSize();
-
         auto menu = CCMenu::create();
 
         auto corner1 = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
-        corner1->setPosition(CCPoint(CCSize(winSize.width * -0.5, winSize.height * -0.5)));
-        corner1->setAnchorPoint(CCPoint(0,0));
-        corner1->setID("corner1"); // Set object ID
+        corner1->setPosition(CCPoint(winSize.width * -0.5, winSize.height * -0.5));
+        corner1->setAnchorPoint(CCPoint(0, 0));
+        corner1->setID("corner1");
         menu->addChild(corner1);
 
         auto corner2 = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
-        corner2->setPosition(CCPoint(CCSize(winSize.width * 0.5, winSize.height * -0.5)));
-        corner2->setAnchorPoint(CCPoint(1,0));
+        corner2->setPosition(CCPoint(winSize.width * 0.5, winSize.height * -0.5));
+        corner2->setAnchorPoint(CCPoint(1, 0));
         corner2->setFlipX(true);
-        corner2->setID("corner2"); // Set object ID
+        corner2->setID("corner2");
         menu->addChild(corner2);
 
         auto gameBgSpr = CCSprite::create("game_bg_01_001.png");
         gameBgSpr->setScale(2.f);
         gameBgSpr->setZOrder(-1);
         gameBgSpr->setPositionY(winSize.height / -2);
-        gameBgSpr->setID("gameBgSpr"); // Set object ID
+        gameBgSpr->setID("gameBgSpr");
         menu->addChild(gameBgSpr);
 
         auto squareSpr = CCScale9Sprite::create("GJ_square01.png");
         squareSpr->setPosition(CCPoint(winSize.width * 0, winSize.height * 0));
         squareSpr->setContentSize(CCPoint(winSize.width * 0.8, winSize.height * 0.8));
-        squareSpr->setID("squareSpr"); // Set object ID
+        squareSpr->setID("squareSpr");
         squareSpr->setZOrder(1);
         menu->addChild(squareSpr);
 
@@ -143,19 +122,19 @@ public:
             menu_selector(GPFeedbackLayer::onClick)
         );
         btn->setPosition(winSize.width * -0.45, winSize.height * 0.4);
-        btn->setID("back-btn"); // Set object ID
+        btn->setID("back-btn");
         menu->addChild(btn);
 
         auto title = CCLabelBMFont::create("Feedback", "goldFont.fnt");
         title->setZOrder(1);
         title->setPositionY(winSize.width * 0.185);
-        title->setID("title"); // Set object ID
+        title->setID("title");
         menu->addChild(title);
 
         auto descBg = CCScale9Sprite::create("square02b_001.png");
         descBg->setPosition(CCPoint(winSize.width * 0.5, winSize.height * 0.5));
         descBg->setContentSize(CCPoint(winSize.width * 0.6, winSize.height * 0.5));
-        descBg->setID("descBg"); // Set object ID
+        descBg->setID("descBg");
         descBg->setZOrder(1);
         descBg->setColor(ccColor3B(0, 0, 0));
         descBg->setOpacity(60);
@@ -164,11 +143,10 @@ public:
         auto descInput = CCTextInputNode::create(winSize.width * 0.6, winSize.height * 0.5, "Type your feedback or feature request here...", "chatFont.fnt");
         descInput->setPosition(CCPoint(winSize.width * 0.5, winSize.height * 0.5));
         descInput->setZOrder(2);
-        descInput->setID("descInput"); // Set object ID
-        descInput->setAllowedChars(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"); // Allow all ASCII characters
-        descInput->setMaxLabelWidth(winSize.width * 0.6); // Maximum width for text input
+        descInput->setID("descInput");
+        descInput->setAllowedChars(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+        descInput->setMaxLabelWidth(winSize.width * 0.6);
         this->addChild(descInput);
-
 
         auto submitSpr = ButtonSprite::create("Submit");
         auto submitBtn = CCMenuItemSpriteExtra::create(
@@ -176,24 +154,29 @@ public:
         );
         submitBtn->setPosition(CCPoint(winSize.width * 0, winSize.height * -0.325));
         submitBtn->setZOrder(1);
-        submitBtn->setID("submit-btn"); // Set object ID
+        submitBtn->setID("submit-btn");
         menu->addChild(submitBtn);
 
         this->addChild(menu);
-
         return true;
     }
 
-    void onClick(CCObject* sender)
-    {
+    void onClick(CCObject* sender) {
         auto scenePrev = CCTransitionFade::create(0.5f, GPFeedbackLayer::garageScene());
         CCDirector::sharedDirector()->replaceScene(scenePrev);
     }
+
+private:
+    geode::EventListener<web::WebTask> m_listener;
 };
 
 class MyLayer : public CCLayer
 {
 public:
+
+    struct Fields {
+        EventListener<web::WebTask> m_listener;
+    };
 
     static CCScene* scene()
     {
@@ -241,6 +224,10 @@ public:
 };
 
 class $modify(GJGarageLayer) {
+    struct Fields {
+        EventListener<web::WebTask> m_listener;
+    };
+
 	bool init() {
 		if(!GJGarageLayer::init()) {
 			return false;
@@ -292,59 +279,68 @@ class $modify(GJGarageLayer) {
             
             if (accID != 0) {
                 // Fetch creator points asynchronously with improved error handling
-                web::AsyncWebRequest()
-                    .bodyRaw(fmt::format("secret=Wmfd2893gb7&targetAccountID={}", accID))
-                    .userAgent("")
-                    .method("POST")
-                    .fetch("https://www.boomlings.com/database/getGJUserInfo20.php")
-                    .text()
-                    .then([=](std::string const& response) {
-                        // Find the position of ":8:"
-                        auto start_pos = response.find(":8:");
-                        if (start_pos == std::string::npos) {
-                            // ":8:" not found, handle the error
-                            std::cerr << "Failed to find ':8:' in response: " << response << std::endl;
-                            return;
-                        }
-                        
-                        // Find the position of the next ':'
-                        auto end_pos = response.find(":", start_pos + 3); // Start searching from the character after ":8:"
-                        if (end_pos == std::string::npos) {
-                            // ':' not found, handle the error
-                            std::cerr << "Failed to find ':' after ':8:' in response: " << response << std::endl;
-                            return;
-                        }
-                        
-                        // Extract the substring between ":8:" and the following ":"
-                        std::string pointsStr = response.substr(start_pos + 3, end_pos - start_pos - 3);
+                web::WebRequest request;
 
-                        // Extract creator points (assuming they are integers)
-                        int creatorPoints;
-                        try {
-                            creatorPoints = std::stoi(pointsStr);
-                            // Create label and add it to the scene
-                            auto cpText = CCLabelBMFont::create(pointsStr.c_str(), "bigFont.fnt");
-                            cpText->setPosition(CCPoint(demonIcon->getPositionX() - 12, demonIcon->getPositionY()));
-                            cpText->setScale(0.34);
-                            cpText->setAnchorPoint({1, 0.5});
-                            cpText->setID("cp-label");
-                            this->addChild(cpText);
-                            this->updateLayout();
-                        } catch (const std::invalid_argument& e) {
-                            std::cerr << "Failed to convert creator points to integer: " << e.what() << std::endl;
-                            auto cpText = CCLabelBMFont::create("Error", "bigFont.fnt");
-                            cpText->setPosition(CCPoint(demonIcon->getPositionX() - 12, demonIcon->getPositionY()));
-                            cpText->setScale(0.34);
-                            cpText->setAnchorPoint({1, 0.5});
-                            cpText->setID("cp-label");
-                            this->addChild(cpText);
-                            this->updateLayout();
-                            return;
-                        }
-                    })
-                    .expect([](std::string const& error) {
-                        std::cerr << "Failed to fetch creator points: " << error << std::endl;
-                    });
+        // Setup the request
+        request.bodyString(fmt::format("secret=Wmfd2893gb7&targetAccountID={}", accID));
+        request.userAgent(""); // Optionally set a user agent
+        request.header("Content-Type", "application/x-www-form-urlencoded"); // Assuming the content type
+        // request.timeout(30); // Set a timeout duration in seconds
+
+        // Define the URL
+        std::string url = "https://www.boomlings.com/database/getGJUserInfo20.php";
+
+        auto cpText = CCLabelBMFont::create("", "bigFont.fnt"); // Create label with an empty string for now
+float demonIconPosX = demonIcon->getPositionX(); // Capture demonIcon's position X
+float demonIconPosY = demonIcon->getPositionY(); // Capture demonIcon's position Y
+
+std::string pointsStr; // Declare pointsStr here
+
+m_fields->m_listener.bind([this, demonIconPosX, demonIconPosY, &pointsStr,  cpText](web::WebTask::Event* e) {
+    if (web::WebResponse* res = e->getValue()) {
+        if (res->ok()) {
+            std::string responseBody = res->string().unwrap();
+            // Process the response
+            auto start_pos = responseBody.find(":8:");
+            if (start_pos != std::string::npos) {
+                auto end_pos = responseBody.find(":", start_pos + 3);
+                if (end_pos != std::string::npos) {
+                    pointsStr = responseBody.substr(start_pos + 3, end_pos - start_pos - 3); // Update pointsStr
+                    try {
+                        int creatorPoints = std::stoi(pointsStr);
+                        // Update label with pointsStr
+                        cpText->setString(pointsStr.c_str());
+                        cpText->setPosition(CCPoint(demonIconPosX - 12, demonIconPosY));
+                        cpText->setScale(0.34);
+                        cpText->setAnchorPoint({1, 0.5});
+                        cpText->setID("cp-label");
+                        this->addChild(cpText);
+                        this->updateLayout();
+                    } catch (const std::invalid_argument& e) {
+                        std::cerr << "Failed to convert creator points to integer: " << e.what() << std::endl;
+                        // Handle error
+                    }
+                } else {
+                    std::cerr << "Failed to find ':' after ':8:' in response: " << responseBody << std::endl;
+                    // Handle error
+                }
+            } else {
+                std::cerr << "Failed to find ':8:' in response: " << responseBody << std::endl;
+                // Handle error
+            }
+        } else {
+            std::cerr << "Request failed with status code: " << res->code() << std::endl;
+            // Handle error
+        }
+    } else if (web::WebProgress* progress = e->getProgress()) {
+        // std::cout << "Request in progress: " << progress->downloadProgress() << std::endl;
+    } else if (e->isCancelled()) {
+        std::cerr << "The request was cancelled... So sad :(" << std::endl;
+    }
+});
+
+        // Set the filter to the request task
+        m_fields->m_listener.setFilter(request.post(url));
             }
         }
 
