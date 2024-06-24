e27013abad121dd1b99c8cc41df646d2e05b0364
0mgRod <rodrigofonseca20@outlook.com>
2024-06-24 07:13:03 +0100

    code backup - dont mind this

--- src/GPFeedbackLayer.hpp
+++ src/GPFeedbackLayer.hpp
@@ -1,30 +1,211 @@
-#ifndef GP_FEEDBACK_LAYER_HPP
-#define GP_FEEDBACK_LAYER_HPP
-
 #include <chrono>
+#include <iostream>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GJGarageLayer.hpp>
 #include <Geode/binding/GJGarageLayer.hpp>
 #include <Geode/utils/web.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 #include <Geode/ui/Notification.hpp>
+// #include "MyLayer.hpp"
 
 using namespace geode::prelude;
 
 class GPFeedbackLayer : public CCLayer {
 public:
-    static CCScene* scene();
-    static CCScene* garageScene();
-    static GPFeedbackLayer* create();
+    static CCScene* scene() {
+        auto scene = CCScene::create();
+        scene->addChild(GPFeedbackLayer::create());
+        return scene;
+    }
 
-    virtual void keyBackClicked();
-    void onFeedbackClick(CCObject* sender);
-    bool init();
-    void onClick(CCObject* sender);
-    void onInfoClick(CCObject* sender);
+    static CCScene* garageScene() {
+        auto scene = CCScene::create();
+        scene->addChild(GJGarageLayer::scene());
+        return scene;
+    }
+
+    static GPFeedbackLayer* create() {
+        GPFeedbackLayer* ret = new GPFeedbackLayer();
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        delete ret;
+        return nullptr;
+    }
+
+    virtual void keyBackClicked() {
+        onClick(nullptr);
+    }
+
+    void onFeedbackClick(CCObject* sender) {
+    auto descInput = dynamic_cast<CCTextInputNode*>(this->getChildByID("descInput"));
+    if (!descInput)
+        return;
+
+    std::string feedback = descInput->getString();
+    std::string username = "Anonymous";
+
+    if (GJAccountManager::get()->m_accountID != 0) {
+        username = std::string(GJAccountManager::get()->m_username.c_str());
+    }
+
+    if (feedback.empty()) {
+        log::error("Feedback cannot be empty.");
+        geode::createQuickPopup(
+            "Garage Plus",
+            "Feedback cannot be empty.",
+            "OK", nullptr,
+            nullptr // No action on Exit button click
+        );
+        return;
+    }
+
+    std::string payload = "{\"username\": \"" + username + "\", \"feedback\": \"" + feedback + "\"}";
+
+web::WebRequest request;
+request.header("Content-Type", "application/json");
+request.bodyString(payload);
+request.timeout(std::chrono::seconds(30));
+
+std::string url = "https://script.google.com/macros/s/AKfycbypWwhGgrokYp1_5SaOw6Pp_Y2_9XuZh5ayXIFncIrvK0uTmvZmrQ6ff1f2bYFYVj2Vgg/exec"; // Replace with your Google Apps Script web app URL
+
+m_listener.bind([this](web::WebTask::Event* e) {
+    if (web::WebResponse* res = e->getValue()) {
+        if (res->ok()) {
+            log::debug("Feedback sent successfully!");
+            geode::createQuickPopup(
+                "Garage Plus",
+                "Feedback sent successfully!",
+                "Exit", nullptr,
+                [this](auto, bool btn1) {
+                    this->onClick(nullptr);
+                }
+            );
+        } else {
+            log::error("Request failed with status code: {}", res->code());
+            log::error("Response body: {}", res->string().unwrapOr("No response body"));
+        }
+    } else if (web::WebProgress* progress = e->getProgress()) {
+        // log::debug("Progress: ", progress->downloadProgress());
+    } else if (e->isCancelled()) {
+        log::error("The request was cancelled.");
+    }
+});
+
+log::debug("Sending POST request to URL: {}", url);
+m_listener.setFilter(request.post(url));
+}
+
+    bool init() {
+        if (!CCLayer::init())
+            return false;
+
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        auto menu = CCMenu::create();
+
+        auto corner1 = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
+        corner1->setPosition(CCPoint(winSize.width * -0.5, winSize.height * -0.5));
+        corner1->setAnchorPoint(CCPoint(0, 0));
+        corner1->setID("corner1");
+        menu->addChild(corner1);
+
+        auto corner2 = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
+        corner2->setPosition(CCPoint(winSize.width * 0.5, winSize.height * -0.5));
+        corner2->setAnchorPoint(CCPoint(1, 0));
+        corner2->setFlipX(true);
+        corner2->setID("corner2");
+        menu->addChild(corner2);
+
+        auto gameBgSpr = CCSprite::create("game_bg_01_001.png");
+        gameBgSpr->setScale(2.f);
+        gameBgSpr->setZOrder(-1);
+        gameBgSpr->setPositionY(winSize.height / -2);
+        gameBgSpr->setID("gameBgSpr");
+        menu->addChild(gameBgSpr);
+
+        auto squareSpr = CCScale9Sprite::create("GJ_square01.png");
+        squareSpr->setPosition(CCPoint(winSize.width * 0, winSize.height * 0));
+        squareSpr->setContentSize(CCPoint(winSize.width * 0.8, winSize.height * 0.8));
+        squareSpr->setID("squareSpr");
+        squareSpr->setZOrder(1);
+        menu->addChild(squareSpr);
+
+        auto backBtn = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"),
+            this,
+            menu_selector(GPFeedbackLayer::onClick)
+        );
+        backBtn->setPosition(winSize.width * -0.45, winSize.height * 0.4);
+        backBtn->setID("back-btn");
+        menu->addChild(backBtn);
+
+        auto infoBtn = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png"),
+            this,
+            menu_selector(GPFeedbackLayer::onInfoClick)
+        );
+        infoBtn->setPosition(winSize.width * 0.45, winSize.height * 0.45);
+        infoBtn->setID("info-btn");
+        menu->addChild(infoBtn);
+
+        auto title = CCLabelBMFont::create("Garage Plus Feedback", "goldFont.fnt");
+        title->setZOrder(1);
+        title->setPositionY(winSize.width * 0.185);
+        title->setID("title");
+        menu->addChild(title);
+
+        auto descBg = CCScale9Sprite::create("square02b_001.png");
+        descBg->setPosition(CCPoint(winSize.width * 0.5, winSize.height * 0.5));
+        descBg->setContentSize(CCPoint(winSize.width * 0.6, winSize.height * 0.5));
+        descBg->setID("descBg");
+        descBg->setZOrder(1);
+        descBg->setColor(ccColor3B(0, 0, 0));
+        descBg->setOpacity(60);
+        this->addChild(descBg);
+
+        auto descInput = CCTextInputNode::create(winSize.width * 0.6, winSize.height * 0.5, "Type your feedback or feature request here...", "chatFont.fnt");
+        descInput->setPosition(CCPoint(winSize.width * 0.5, winSize.height * 0.5));
+        descInput->setZOrder(2);
+        descInput->setID("descInput");
+        descInput->setAllowedChars(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+        descInput->setMaxLabelWidth(winSize.width * 0.6);
+        this->addChild(descInput);
+
+        auto submitSpr = ButtonSprite::create("Submit");
+        auto submitBtn = CCMenuItemSpriteExtra::create(
+            submitSpr, this, menu_selector(GPFeedbackLayer::onFeedbackClick)
+        );
+        submitBtn->setPosition(CCPoint(winSize.width * 0, winSize.height * -0.325));
+        submitBtn->setZOrder(1);
+        submitBtn->setID("submit-btn");
+        menu->addChild(submitBtn);
+
+        this->addChild(menu);
+        return true;
+    }
+
+    void onClick(CCObject* sender) {
+        auto scenePrev = CCTransitionFade::create(0.5f, GPFeedbackLayer::garageScene());
+        CCDirector::sharedDirector()->replaceScene(scenePrev);
+    }
+
+    void onInfoClick(CCObject* sender) {
+auto winSize = CCDirector::sharedDirector()->getWinSize();
+        FLAlertLayer::create(
+    nullptr, // No delegate needed
+    "Garage Plus", // Title
+    "By submitting your feedback, you agree to the collection and transmission of your username and feedback to a private spreadsheet accessible only to the developer of this mod, OmgRod. This data will be used solely for improving the mod.\n\nYou may disable the feedback button in this mod's settings at any time to add extra space.\n\nYour data will be stored securely and will not be shared with third parties.\n\nThank you for your feedback!\n\n-OmgRod", // Description
+    "OK", // Button 1 (OK)
+    nullptr, // Button 2 (no second button)
+    winSize.width * 0.75, // Width (adjust as necessary)
+    true, // Scrolling enabled
+    winSize.height * 0.75, // Height (adjust as necessary)
+    1.0f // Text scale (adjust as necessary)
+)->show();
+
+    }
 
 private:
     geode::EventListener<web::WebTask> m_listener;
 };
-
-#endif // GP_FEEDBACK_LAYER_HPP
--- src/MyLayer.hpp
+++ src/MyLayer.hpp
@@ -1,7 +1,5 @@
-#ifndef MY_LAYER_HPP
-#define MY_LAYER_HPP
-
 #include <chrono>
+#include <iostream>
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GJGarageLayer.hpp>
 #include <Geode/binding/GJGarageLayer.hpp>
@@ -11,20 +9,84 @@
 
 using namespace geode::prelude;
 
-class MyLayer : public CCLayer {
+class MyLayer : public CCLayer
+{
 public:
-    static CCScene* scene();
-    static CCScene* garageScene();
-    static MyLayer* create();
-
-    virtual void keyBackClicked();
-    void onFeedbackClick(CCObject* sender);
-    bool init();
-    void onClick(CCObject* sender);
-    void onInfoClick(CCObject* sender);
+    struct Fields {
+        EventListener<web::WebTask> m_listener;
+    };
 
 private:
-    geode::EventListener<web::WebTask> m_listener;
-};
+    Fields* m_fields;
 
-#endif // MY_LAYER_HPP
+public:
+    MyLayer() : m_fields(new Fields()) {}
+
+    ~MyLayer() {
+        delete m_fields;
+    }
+
+    static CCScene* scene()
+    {
+        auto scene = CCScene::create();
+        scene->addChild(MyLayer::create());
+        return scene;
+    }
+
+    static MyLayer* create()
+    {
+        MyLayer* ret = new MyLayer();
+        if (ret && ret->init())
+        {
+            ret->autorelease();
+            return ret;
+        }
+        delete ret;
+        return nullptr;
+    }
+
+    bool init()
+    {
+        if (!CCLayer::init())
+            return false;
+
+        return true;
+    }
+
+    void onClick(CCObject* sender)
+    {
+        auto scenePrev = CCTransitionFade::create(0.5f, MyLayer::scene());
+        CCDirector::sharedDirector()->replaceScene(scenePrev);
+    }
+
+    void onModSettings(CCObject* sender)
+    {
+        openSettingsPopup(Mod::get());
+    }
+
+    void onFeedbackBtn(CCObject* sender)
+    {
+        auto scenePrev = CCTransitionFade::create(0.5f, GPFeedbackLayer::scene());
+        CCDirector::sharedDirector()->replaceScene(scenePrev);
+    }
+
+    void demonInfo(CCObject* sender)
+    {
+        FLAlertLayer::create("Garage Plus", "This feature may or may not be coming soon", "OK")->show();
+    }
+
+    void starsInfo(CCObject* sender)
+    {
+        FLAlertLayer::create("Garage Plus", "This feature may or may not be coming soon", "OK")->show();
+    }
+
+    void moonsInfo(CCObject* sender)
+    {
+        FLAlertLayer::create("Garage Plus", "This feature may or may not be coming soon", "OK")->show();
+    }
+
+    void onDisabled(CCObject* sender)
+    {
+        FLAlertLayer::create("Garage Plus", "This feature is currently disabled possibly due to a bug.", "OK")->show();
+    }
+};
