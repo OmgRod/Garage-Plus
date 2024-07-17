bd6e991b9aa86957ae8edf2d687d847f47dd5399
0mgRod <rodrigofonseca20@outlook.com>
2024-07-17 18:58:54 +0100

    v2.1.0

--- src/GPKofiLayer.hpp
+++ src/GPKofiLayer.hpp
@@ -54,20 +54,65 @@ public:
         auto winSize = CCDirector::sharedDirector()->getWinSize();
         auto menu = CCMenu::create();
 
-        // geode::utils::web::openLinkInBrowser("https://ko-fi.com/omgrod");
+        auto title = CCLabelBMFont::create("Support OmgRod (Garage Plus)", "goldFont.fnt");
+        title->setZOrder(1);
+        title->setPositionX(winSize.width * 0.5);
+        title->setPositionY(winSize.height * 0.85);
+        title->setID("title");
+        this->addChild(title);
+
+        auto gameBgSpr = CCSprite::create("game_bg_01_001.png");
+        gameBgSpr->setScale(2.f);
+        gameBgSpr->setZOrder(-99999);
+        gameBgSpr->setPositionY(winSize.height / -2);
+        gameBgSpr->setPositionX(winSize.width / 2);
+        gameBgSpr->setID("gameBgSpr");
+        this->addChild(gameBgSpr);
+        
         std::filesystem::path videoPath = Mod::get()->getResourcesDir() / "kofiPromo.mpg";
 
         // Create a VideoPlayer instance
         auto player = videoplayer::VideoPlayer::create(videoPath, true);
 
-        player->setWidth(0.7 * winSize.width);
-
+        player->setHeight(0.9 * winSize.height * 9 / 16);
         player->setPosition(winSize.width * 0.5, winSize.height * 0.5);
 
-        this->addChild(player);
+        auto linkBtnSpr = ButtonSprite::create("Open Ko-fi");
+        auto linkBtn = CCMenuItemSpriteExtra::create(
+            linkBtnSpr, this, menu_selector(GPKofiLayer::onKofiBtn)
+        );
+        linkBtn->setPositionY(winSize.height * -0.4);
+        menu->addChild(linkBtn);
 
+        auto backBtn = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_arrow_01_001.png"),
+            this,
+            menu_selector(GPFeedbackLayer::onClick)
+        );
+        backBtn->setPosition(winSize.width * -0.45, winSize.height * 0.4);
+        backBtn->setID("back-btn");
+        menu->addChild(backBtn);
+
+        auto corner1 = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
+        corner1->setAnchorPoint(CCPoint(0, 0));
+        corner1->setID("corner1");
+        this->addChild(corner1);
+
+        auto corner2 = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
+        corner2->setPositionX(winSize.width);
+        corner2->setAnchorPoint(CCPoint(1, 0));
+        corner2->setFlipX(true);
+        corner2->setID("corner2");
+        this->addChild(corner2);
+
+
+        this->addChild(player);
         this->addChild(menu);
         return true;
+    }
+
+    void onKofiBtn(CCObject* sender) {
+        geode::utils::web::openLinkInBrowser("https://ko-fi.com/omgrod");
     }
 
 private:
